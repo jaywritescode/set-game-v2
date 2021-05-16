47b170d1ae627a927df6fe57b674aa6796a4b23c
Jay Harris <jaywritescode@users.noreply.github.com>
2021-05-16 13:18:48 -0400

    onSelectCard is responsible for adding or removing card to selected list, separate effect for checking for set

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -35,32 +35,44 @@ const isSet = (cards) => {
 
 function App() {
   const [board, setBoard] = useState([]);
-  const [selected, setSelected] = useState([]);
-
   useEffect(async () => {
     const response = await fetch('/start');
     const result = await response.json();
     setBoard(result);
   }, []);
 
-  const onSelectCard = (card) => {
-    if (_.find(selected, card)) {
-      setSelected(produce((draft) => _.reject(draft, card)));
-    } else if (selected.length < 2) {
-      setSelected(
-        produce((draft) => {
-          draft.push(card);
-        }),
-      );
-      return;
-    } else {
-      if (isSet(selected.concat(card))) {
-        console.log('isSet');
-      }
+  const [selected, setSelected] = useState([]);
+  useEffect(async () => {
+    if (isSet(selected)) {
+      const response = await fetch('/submit', {
+        method: 'PUT',
+        cache: 'no-cache',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(selected),
+      });
+      console.log(response);  
+    }
 
+    if (selected.length == 3) {
       setSelected([]);
     }
-  };
+  }, [selected]);
+
+
+  const onSelectCard = (card) => {
+    if (isSelected(card)) {
+      setSelected(produce((draft) => _.reject(draft, card)));
+      return;
+    }
+
+    setSelected(produce((draft) => draft.concat(card)));
+  }
+
+
+
+  const isSelected = (card) => _.find(selected, card);
+
+
 
   return (
     <div className="container">
