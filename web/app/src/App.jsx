1976f993a11138571ea72c0d6b65a956dc3d28e1
Jay Harris <jaywritescode@users.noreply.github.com>
2021-05-25 15:32:31 -0400

    include useWebSocket hook

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import useWebSocket from 'react-use-websocket';
 
 import Game from './Game.jsx';
 
+const socketUrl = 'ws://0.0.0.0:8899/ws';
+
 export default function App() {
+  const {
+    sendJsonMessage,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('opened'),
+  });
+
   return (
     <Game />
   );
