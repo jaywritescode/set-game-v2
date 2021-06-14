6108138319973f202c04361f6e393854cc566e5d
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-13 20:50:35 -0400

    click "create new game" button sends request

--- web/app/src/App.test.jsx
+++ web/app/src/App.test.jsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('does something', () => {
+  it('creates a new game', () => {
     const { getByRole } = render(<App />);
     const createGameButton = getByRole('button', { name: 'create new game' });
+
+    userEvent.click(createGameButton);
     expect(document.body.contains(createGameButton));
   });
 });
