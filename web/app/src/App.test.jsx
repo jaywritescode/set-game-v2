587820ee99331389cde5ec644a491c0840cd3d00
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-16 19:00:08 -0400

    clean up

--- web/app/src/App.test.jsx
+++ web/app/src/App.test.jsx
@@ -2,18 +2,19 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
-import { rest, setupWorker } from 'msw'
+import { rest, setupWorker } from 'msw';
 import App from './App';
 
+const ROOM = 'aaaa';
 const worker = setupWorker(
   rest.post('/api/create', (req, res, ctx) => {
-    return res(ctx.json({ room: 'aaaa' }))
+    return res(ctx.json({ room: ROOM }));
   }),
 
   rest.get('/api/join', (req, res, ctx) => {
-    return res(ctx.json({ 'room': req.url.searchParams.get('room') }))
+    return res(ctx.json({ room: req.url.searchParams.get('room') }));
   }),
-)
+);
 
 before(() => worker.start());
 afterEach(() => worker.resetHandlers());
@@ -26,18 +27,18 @@ describe('<App>', () => {
     userEvent.click(createGameButton);
     const roomCodeElement = await findByTestId('room-code');
 
-    expect(roomCodeElement.textContent).to.contain('aaaa');
+    expect(roomCodeElement.textContent).to.contain(ROOM);
   });
 
   it('joins a game', async () => {
     const { getByRole, getByLabelText, findByTestId } = render(<App />);
-    const joinGameButton = getByRole('button', { name: 'join game'});
+    const joinGameButton = getByRole('button', { name: 'join game' });
     const roomCodeInput = getByLabelText(/room code/i, { selector: 'input' });
 
-    userEvent.type(roomCodeInput, 'aaaa');
+    userEvent.type(roomCodeInput, ROOM);
     userEvent.click(joinGameButton);
     const roomCodeElement = await findByTestId('room-code');
 
-    expect(roomCodeElement.textContent).to.contain('aaaa');
-  })
+    expect(roomCodeElement.textContent).to.contain(ROOM);
+  });
 });
