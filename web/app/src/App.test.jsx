a4b60725368413493a97d54f16dfba37a378fdbf
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-16 18:58:49 -0400

    tests seem to be working. no more "after"?

--- web/app/src/App.test.jsx
+++ web/app/src/App.test.jsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
-import worker from './mocks/browser';
+import { rest, setupWorker } from 'msw'
 import App from './App';
 
-before(() => {
-  worker.start();
-});
+const worker = setupWorker(
+  rest.post('/api/create', (req, res, ctx) => {
+    return res(ctx.json({ room: 'aaaa' }))
+  }),
 
-after(() => {
-  worker.stop();
-});
+  rest.get('/api/join', (req, res, ctx) => {
+    return res(ctx.json({ 'room': req.url.searchParams.get('room') }))
+  }),
+)
+
+before(() => worker.start());
+afterEach(() => worker.resetHandlers());
 
 describe('<App>', () => {
-  afterEach(() => {
-    worker.resetHandlers();
-  });
-
   it('creates a new game', async () => {
     const { getByRole, findByTestId } = render(<App />);
     const createGameButton = getByRole('button', { name: 'create new game' });
@@ -27,4 +28,16 @@ describe('<App>', () => {
 
     expect(roomCodeElement.textContent).to.contain('aaaa');
   });
+
+  it('joins a game', async () => {
+    const { getByRole, getByLabelText, findByTestId } = render(<App />);
+    const joinGameButton = getByRole('button', { name: 'join game'});
+    const roomCodeInput = getByLabelText(/room code/i, { selector: 'input' });
+
+    userEvent.type(roomCodeInput, 'aaaa');
+    userEvent.click(joinGameButton);
+    const roomCodeElement = await findByTestId('room-code');
+
+    expect(roomCodeElement.textContent).to.contain('aaaa');
+  })
 });
