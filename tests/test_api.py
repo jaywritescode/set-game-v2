97575199dc7676174f64d677a9a369d252ce095a
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-18 14:40:26 -0400

    more api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,45 @@
+import pytest
 from starlette.testclient import TestClient
 
 from web.api import app
 from game.setgame import Game
 
-def test_create():
-  client = TestClient(app)
+
+@pytest.fixture
+def client():
+  with TestClient(app) as test_client:
+    yield test_client
+    test_client.app.state.GAMES = dict()
+
+
+def test_create(client):
   response = client.post('/create')
 
   room_code = response.json().get('room')
   assert room_code is not None
   assert isinstance(app.state.GAMES[room_code], Game)
+
+
+def test_join_success(client):
+  room_code = 'abcd'
+  app.state.GAMES[room_code] = Game()
+
+  response = client.get(f"/join?room={room_code}")
+  assert response.json() == { 'room': room_code }
+
+
+def test_join_failure(client):
+  response = client.get(f"/join?room=abcd")
+  assert response.status_code == 404
+
+
+def test_start_success(client):
+  response = client.post('/create')
+  room_code = response.json().get('room')
+
+  response = client.get(f"/start/{room_code}")
+  body = response.json()
+
+  assert body.get('type') == 'start-game'
+  assert body.get('payload')
+  # TODO: assert body.get('payload') matches expected schemas
