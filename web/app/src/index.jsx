d2c48eb87976371b09e3c3ce00ba6c7859390058
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-27 15:25:05 -0400

    use CssBaseline

--- web/app/src/index.jsx
+++ web/app/src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import App from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
