9cdf010171906c42f6c3b0b740563ab1abca0fd3
Jay Harris <jaywritescode@users.noreply.github.com>
2021-06-27 15:16:40 -0400

    fetch player's name

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -8,9 +8,19 @@ export const ROOM_CODE_LENGTH = 4;
 
 export default function App() {
   const [room, setRoom] = useState(null);
+  const [player, setPlayer] = useState(null);
 
   if (!room) {
     return (<Entry setRoom={setRoom} />);
+  }
+  else if (!player) {
+    return (
+      <>
+        <label htmlFor="player_name">Who are you?</label>
+        <input type="text" name="player_name" />
+        <button>OK</button>
+      </>
+    );
   }
 
   return <Room id={room} />;
