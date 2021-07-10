906e84801e47c6e7a03a57e0f93e6916fe09aea6
Jay Harris <jaywritescode@users.noreply.github.com>
2021-07-10 15:33:52 -0400

    add second test, also works

--- web/app/src/App.test.jsx
+++ web/app/src/App.test.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
   it('renders create new game button', () => {
     const { getByRole } = render(<App />);
     getByRole('button', { name: /create new game/i });
+  });
+
+  it('renders join game button', () => {
+    const { getByRole } = render(<App />);
+    getByRole('button', { name: /join game/i });
   });
 });
