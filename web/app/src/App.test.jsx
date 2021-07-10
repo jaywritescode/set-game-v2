ec0311eda6c3c0bbc801c8ce1c153cdb02ac40d5
Jay Harris <jaywritescode@users.noreply.github.com>
2021-07-10 17:24:21 -0400

    test join game button enabled/disabled

--- web/app/src/App.test.jsx
+++ web/app/src/App.test.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
@@ -8,8 +10,19 @@ describe('<App>', () => {
     getByRole('button', { name: /create new game/i });
   });
 
-  it('renders join game button', () => {
-    const { getByRole } = render(<App />);
-    getByRole('button', { name: /join game/i });
+  describe('join game button', () => {
+    it('is disabled', () => {
+      const { getByRole } = render(<App />);
+      const joinButton = getByRole('button', { name: /join game/i });
+      expect(joinButton.disabled).to.be.ok;
+    });
+
+    it('is enabled with a valid room code', () => {
+      const { getByRole } = render(<App />);
+      const joinButton = getByRole('button', { name: /join game/i });
+
+      userEvent.type(getByRole('textbox', { name: /room code/i }), 'abcd');
+      expect(joinButton.disabled).to.be.false;
+    });
   });
 });
